869bfbb35023baae6cc859843011365c44e39436
Nikita R <rdnkta@gmail.com>
2019-01-08 23:44:00 +0200

    improved logic

--- js/script.js
+++ js/script.js
@@ -1,39 +1,35 @@
 const mainInfo = document.getElementsByClassName('main-info');
-let show = document.getElementsByClassName('show');
-let active = document.getElementsByClassName('active');
 const searchbtn = document.getElementById('search-btn');
 const searchInput = document.getElementById('input');
 let searchOutput = document.getElementById('output');
-let i;
 
 searchbtn.addEventListener('click', findSong);
 
 function findSong() {
-  let inputValue = searchInput.value;
-  inputValue = inputValue.replace(/\s/gi, '+');
-  let url = `https://itunes.apple.com/search?term=${inputValue}&limit=10&entity=musicTrack`;
+	let inputValue = searchInput.value;
+	inputValue = inputValue.replace(/\s/gi, '+');
+	let url = `https://itunes.apple.com/search?term=${inputValue}&limit=10&entity=musicTrack`;
 
-  fetch(url)
-    .then(function(response) {
-      if (response.status !== 200) {
-        console.log(response.status);
-        return;
-      }
-      return response.json();
-    })
-    .then(json => showResults(json));
+	fetch(url)
+		.then(function(response) {
+			if (response.status !== 200) {
+				console.log(response.status);
+				return;
+			}
+			return response.json();
+		})
+		.then(json => showResults(json));
 }
 
 function showResults(json) {
-  searchOutput.innerHTML = '';
-  json.results.forEach((item, key) => {
-    searchOutput.innerHTML += `<div id="item${key}" class="search-result container 
+	searchOutput.innerHTML = '';
+	json.results.forEach((item, key) => {
+		searchOutput.innerHTML += `<div id="item${key}" class="search-result container 
     ${key % 2 === 0 ? 'has-background-light' : ''} 
     rounded">
   <div class="main-info columns is-vcentered">
     <div class="column has-text-centered is-2">
-      <img src=${item.artworkUrl100}
-        alt="cover">
+      <img src=${item.artworkUrl100} alt="cover">
     </div>
     <div class="column">
       ${item.artistName}
@@ -81,46 +77,34 @@ function showResults(json) {
   </div>
 </div>
 </section>`;
-  });
+	});
 
-  for (i = 0; i < mainInfo.length; i++) {
-    mainInfo[i].addEventListener('click', function() {
-      const panel = this.nextElementSibling;
+	for (let i = 0; i < mainInfo.length; i++) {
+		mainInfo[i].addEventListener('click', togglePanel);
+	}
+}
 
-      // looking for the +/- switch of this particular mainInfo panel
-      const sign = this.childNodes[11].childNodes[1];
-      sign.textContent = '+';
+function togglePanel(e) {
+	let show = document.querySelector('.show');
+	let active = document.querySelector('.active');
+	const panel = this.nextElementSibling;
 
-      if (this.classList.contains('active')) {
-        this.classList.remove('active');
-      }
-
-      collapseOtherPanels();
-
-      if (!this.classList.contains('active')) {
-        sign.textContent = '-';
-        panel.classList.add('show');
-        this.classList.add('active');
-      }
-    });
-  }
+	if (active == this && show == panel) {
+		this.classList.remove('active');
+		panel.classList.remove('show');
+	} else if (show && active) {
+		show.classList.remove('show');
+		active.classList.remove('active');
+		this.classList.toggle('active');
+		panel.classList.toggle('show');
+	} else {
+		this.classList.toggle('active');
+		panel.classList.toggle('show');
+	}
 }
 
 function millisToMinutesAndSeconds(millis) {
-  let minutes = Math.floor(millis / 60000);
-  let seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-}
-
-function collapseOtherPanels() {
-  // if we have an item with the class .show
-  if (show[0]) {
-    show[0].classList.remove('show');
-    if (active[0]) {
-      const sign = active[0].childNodes[11].childNodes[1];
-      sign.textContent = '+';
-    }
-
-    active[0].classList.remove('active');
-  }
+	let minutes = Math.floor(millis / 60000);
+	let seconds = ((millis % 60000) / 1000).toFixed(0);
+	return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
 }
